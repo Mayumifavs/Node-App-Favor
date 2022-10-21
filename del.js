406ee609dcc462b30b5d38f326d0ada31c5198b6
mayumsfavor <mayumsfavor@gmail.com>
2022-10-21 13:12:14 +0800

    del.js updated

--- del.js
+++ del.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const del = function(id, oldNote){
     const note = JSON.parse(oldNote)
     const newNote = note.filter(function(note, idx){
-            return note.id !== idx
+            return note.id !== id
     })
     fs.writeFileSync('./note.txt', JSON.stringify(newNote))
 }
